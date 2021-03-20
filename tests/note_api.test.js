9a8ad321667c9164af3f636967b7d10a7604764e
Gary Huong <garyhuong@gmail.com>
2021-03-20 18:34:37 -0400

    Test endpoints for viewing specific note, and deleting notes

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -48,7 +48,7 @@ test("a valid note can be added", async () => {
   const notesAtEnd = await helper.notesInDb();
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
 
-  const contents = notesAtEnd.map(n => n.content);
+  const contents = notesAtEnd.map((n) => n.content);
   expect(contents).toContain("async/await simplifies making async calls");
 });
 
@@ -61,6 +61,36 @@ test("note without content is not added", async () => {
 
   const notesAtEnd = await helper.notesInDb();
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+
+test("a specific note can be viewed", async () => {
+  const notesAtStart = await helper.notesInDb();
+
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+
+  expect(resultNote.body).toEqual(processedNoteToView);
+});
+
+test("a note can be deleted", async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map(r => r.content);
+
+  expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(() => {
