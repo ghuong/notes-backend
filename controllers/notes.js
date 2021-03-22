c8f73f88372af4088077e1022da68d04ea78cbf7
Gary Huong <garyhuong@gmail.com>
2021-03-22 08:42:14 -0400

    Modify create note route to store user author, and add note id to user's notes in db

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 // API Routes
 
@@ -23,6 +24,10 @@ notesRouter.get("/:id", async (request, response) => {
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const user = body.userId
+    ? await User.findById(body.userId)
+    : null;
+
   if (body.content === "undefined") {
     return response.status(400).json({
       error: "content missing",
@@ -33,9 +38,14 @@ notesRouter.post("/", async (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user?._id
   });
 
   const savedNote = await note.save();
+  if (user)
+    user.notes = user.notes.concat(savedNote._id);
+  await user?.save();
+  
   response.json(savedNote);
 });
 
