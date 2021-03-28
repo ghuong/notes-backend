15de894cb715c673091e44718de76e23c53abe37
Gary Huong <garyhuong@gmail.com>
2021-03-28 08:30:40 -0400

    Add one hour expiration to JWT

--- utils/middleware.js
+++ utils/middleware.js
@@ -20,12 +20,15 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "Malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  } else if (error.name === "JsonWebTokenError") {
-    return response.status(401).json({ error: "Invalid token" });
+  switch (error.name) {
+    case "CastError":
+      return response.status(400).send({ error: "Malformatted id" });
+    case "ValidationError":
+      return response.status(400).json({ error: error.message });
+    case "JsonWebTokenError":
+      return response.status(401).json({ error: "Invalid token" });
+    case "TokenExpiredError":
+      return response.status(401).json({ error: "Token expired" });
   }
 
   next(error);
